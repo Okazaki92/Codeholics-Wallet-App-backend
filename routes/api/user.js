ed8fb9a3674107350bec0adb75b255f7449b5526
Okazaki92 <vincentslominski@gmail.com>
2023-09-20 22:27:38 +0200

    feat/dodano endpointy, controller usera,

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { signup } = require("../../controller/userController");
+const {
+  signup,
+  login,
+  logout,
+  verifyToken,
+  currentUser,
+  sendVerifyToken,
+} = require("../../controller/userController");
 const { auth } = require("../../middlewares/auth");
 
 router.post("/signup", signup);
+router.post("/login", login);
+router.post("/verify", sendVerifyToken);
+router.get("/logout", auth, logout);
+router.get("/verify/:verificationToken", verifyToken);
+router.get("/current", currentUser);
 
 module.exports = router;
