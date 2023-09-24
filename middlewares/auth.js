ab20a6cdaccf7a08697a650132bad87f9ab4367d
Vincent Słomiński <113301102+Okazaki92@users.noreply.github.com>
2023-09-24 13:02:24 +0200

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ const passport = require("passport");
 const auth = (req, res, next) => {
   const authorization = req.headers.authorization;
   passport.authenticate("jwt", { session: false }, (err, user) => {
-    if (!user || err) {
+    if (!user || err || user.token !== authorization.split(" ")[1]) {
       return res.status(401).json({
         message: "Not authorized",
       });
