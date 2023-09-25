2e58f2156954d741e7888469a44f6d67015357a4
Jacek Pasierb <jack_byk@o2.pl>
2023-09-25 15:38:40 +0200

    Created objectCat with array objects for all category transactions user

--- controller/categoriesController.js
+++ controller/categoriesController.js
@@ -6,11 +6,21 @@ const getCategories = async (req, res, next) => {
 
   const categories = await getCategoriesUser(month, year, userId);
     console.log("category", categories);
+    const objectCat = [];
+    for ( const category of categories) {
+        // console.log("category", category.category);
+        // console.log("sum", category.sum);
+        const Cat = category.category;
+        const Sum = category.sum;
+        objectCat.push({  [Cat]: Sum })
+        console.log('obj',objectCat);
+    }
+
     return res.status(200).json({
       status: "ok",
       code: 200,
       message: "testy",
-      data: { categories },
+        data: { categories },
     });
 };
 
