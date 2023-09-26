05d418d910a90ac9a21f17eb219e83f133ce6099
Jacek Pasierb <jack_byk@o2.pl>
2023-09-26 10:02:11 +0200

    Done function for received arrCategory and Sum expense

--- controller/categoriesController.js
+++ controller/categoriesController.js
@@ -5,23 +5,25 @@ const getCategories = async (req, res, next) => {
   const { month, year } = req.query;
 
   const categories = await getCategoriesUser(month, year, userId);
-    console.log("category", categories);
-    const objectCat = [];
-    for ( const category of categories) {
-        // console.log("category", category.category);
-        // console.log("sum", category.sum);
-        const Cat = category.category;
-        const Sum = category.sum;
-        objectCat.push({  [Cat]: Sum })
-        console.log('obj',objectCat);
+  console.log("Wszystkie transakcje uzytkownika danego miesąca:", categories);
+  const arrCategory = [];
+  for (const category of categories) {
+    const Cat = category.category;
+    const Sum = category.sum;
+    if (!arrCategory.some((ob) => Cat in ob)) {
+      arrCategory.push({ [Cat]: Sum });
+    } else {
+      const edit = arrCategory.find((ob) => Cat in ob);
+      edit[Cat] += Sum;
     }
-
-    return res.status(200).json({
-      status: "ok",
-      code: 200,
-      message: "testy",
-        data: { categories },
-    });
+  }
+  console.log(`Tablica kategori`, arrCategory);
+  return res.status(200).json({
+    status: "ok",
+    code: 200,
+    message: "testy",
+    data: arrCategory,
+  });
 };
 
 module.exports = {
