fd7c18edfba11be92872e35da90adc5650ce3f39
Jacek Pasierb <jack_byk@o2.pl>
2023-10-01 00:54:27 +0200

    Connect swagger, but no operations defined in spec

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 require("./config/passport-config");
-
 const app = express();
+// Konfiguracja Swagger
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "WalletAPI",
+      version: "1.0.1",
+    },
+  },
+  // Ścieżka do pliku z konfiguracją Swagger
+  apis: ["./swagger.json"],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+// Dodanie interfejsu Swagger do ścieżki /api-docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const userRouter = require("./routes/api/user");
 const transactionsRouter = require("./routes/api/transactions");
