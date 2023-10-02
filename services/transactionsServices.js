4a4638712898faa43c524307f497b22f29ec9609
Okazaki92 <vincentslominski@gmail.com>
2023-10-02 02:32:30 +0200

    cofnięto zmiany

--- services/transactionsServices.js
+++ services/transactionsServices.js
@@ -10,9 +10,6 @@ const getTransactions = async (query, userId) => {
     .skip(startIndex)
     .limit(limit);
 };
-const countTransactions = async (userId) => {
-  return Transaction.find({ owner: userId });
-};
 
 const addTransaction = async (userId, body) => {
   return Transaction.create({ owner: userId, ...body });
@@ -36,7 +33,6 @@ const update = async (transactionId, userId, body) => {
 
 module.exports = {
   getTransactions,
-  countTransactions,
   addTransaction,
   removeTransaction,
   getTransactionWithId,
