0dc8717d31e67d130e0be9cb26b01cd5fe81f7a0
Okazaki92 <vincentslominski@gmail.com>
2023-10-03 17:57:27 +0200

    save

--- controller/userController.js
+++ controller/userController.js
@@ -218,7 +218,7 @@ const updateBalanceAfterChange = async (
 ) => {
   let newBalance;
   if (!transactionType) {
-    newBalance = balance + oldTransactionSum - transactionSum;
+    newBalance = balance - oldTransactionSum + transactionSum;
   } else {
     newBalance = balance + oldTransactionSum - transactionSum;
   }
